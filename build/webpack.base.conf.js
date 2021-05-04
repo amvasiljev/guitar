49179ceaae3aa099a46fdab6af6183bb89c5ba7a
bikeol <a@mosam.ru>
2021-05-04 17:14:42 +0300

    up

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,7 +32,7 @@ module.exports = {
     // module: `${PATHS.src}/your-module.js`,
   },
   output: {
-    filename: `${PATHS.assets}js/[name].[hash].js`,
+    filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
     publicPath: '/'
   },
@@ -134,7 +134,7 @@ module.exports = {
   plugins: [
     // new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
+      filename: `${PATHS.assets}css/[name].css`,
     }),
     new CopyWebpackPlugin({
       patterns: [
